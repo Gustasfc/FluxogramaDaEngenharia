d9900068bd80f5ad869d397a4c6b228b625255eb
Gustavo Gonzalez <gustavorgonzalezgja@gmal.com>
2025-05-19 23:36:22 -0300

    fluxogramadaengenharia

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿bool objetoEstaSeMovendo;
+bool objetoDeveriaMover;
+string mensagem;
+
+Console.Clear();
+
+Console.WriteLine("*--- Fluxograma da Engenharia ---*");
+Console.WriteLine("O objeto está se movendo (Digite 's' para sim ou 'n' para não)?");
+objetoEstaSeMovendo = Console.ReadLine()! == "s"; 
+
+Console.WriteLine("Ele deveria estar se movendo (Digite 's' para sim ou 'n' para não)?");
+objetoDeveriaMover = Console.ReadLine()! == "s";
+
+if (!objetoEstaSeMovendo)
+{
+    if (!objetoDeveriaMover)
+    {
+        mensagem = "Ótimo!";
+        Console.WriteLine(mensagem);
+    }
+    else
+    {
+        mensagem = "Use WD-40!";
+        Console.WriteLine(mensagem);
+    }
+}
+else
+{
+    if (!objetoDeveriaMover)
+    {
+        mensagem = "Use Silver Tape!";
+        Console.WriteLine(mensagem);
+    }
+    else
+    {
+        mensagem = "Ótimo!";
+        Console.WriteLine(mensagem);
+    }
+}
